1954b86ab3dfc0e43b3984a335b83f1d26fd4cc9
Elect <elect.m.g@gmail.com>
2022-07-15 17:42:47 +0500

    Python 8-2

---
+++ Python_8/theory_2.py
@@ -0,0 +1,58 @@
+from collections import Counter
+#c = Counter()
+#c ["Red"] += 1
+
+
+#cars = ['red', 'blue', 'black', 'black', 'black', 'red', 'blue', 'red', 'white']
+#c = Counter()
+#for car in cars:
+    #c[car] += 1
+ # ИЛИ
+#c = Counter(cars)
+
+#print(c['black'])
+#print(sum(c.values()))
+
+#ОПЕРАЦИИ С COUNTER
+cars_moscow = ['black', 'black', 'white', 'black', 'black', 'white', 'yellow', 'yellow', 'yellow']
+cars_spb = ['red', 'black', 'black', 'white', 'white', 'yellow', 'yellow', 'red', 'white']
+
+counter_moscow = Counter(cars_moscow)
+counter_spb = Counter(cars_spb)
+
+# Сложение словарей
+#print(counter_moscow + counter_spb)
+
+# Вычитание словарей(При использовании "-" отрицательные значения не возможны)
+#counter_moscow.subtract(counter_spb)
+#print(counter_moscow)
+
+#1. Чтобы получить список всех элементов, которые содержатся в Counter, используется функция elements(). \
+    # Она возвращает итератор, поэтому, чтобы напечатать все элементы, распакуем их с помощью *:
+#print(*counter_moscow.elements())
+
+#2. Чтобы получить список уникальных элементов, достаточно воспользоваться функцией list():
+#print(list(counter_moscow))
+
+# 3. С помощью функции dict() можно превратить Counter в обычный словарь:
+#print(dict(counter_moscow))
+
+# 4. Функция most_common() позволяет получить список из кортежей элементов в порядке убывания их встречаемости:
+#print(counter_moscow.most_common())
+# ИЛИ можно передать значение, которое задаёт желаемое число первых наиболее частых элементов, например, 2:
+#print(counter_moscow.most_common(2))
+
+# 5. Наконец, функция clear() позволяет полностью обнулить счётчик:
+#counter_moscow.clear()
+
+# Задание 2.3
+#print(c.most_common(1))
+
+# Задание 2.4
+#print(c[953421102])
+
+# Задание 2.5
+#from hidden import clients
+#from collections import Counter
+#c = Counter(clients)
+#print(len(list(c)))
